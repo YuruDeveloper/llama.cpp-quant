36e35eee64aa909fd3c7b8458c8f2c7f56e0f04e
YuruDeveloper <vkgkvk8@gmail.com>
2026-04-27 21:19:40 +0900

    maybe fix bug

--- ggml/src/ggml.c
+++ ggml/src/ggml.c
@@ -7940,3 +7940,4 @@ bool ggml_threadpool_params_match(const struct ggml_threadpool_params * p0, cons
     if (p0->strict_cpu != p1->strict_cpu ) return false;
     return memcmp(p0->cpumask, p1->cpumask, GGML_MAX_N_THREADS) == 0;
 }
+
